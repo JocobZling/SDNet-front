43f037cd755d9fc0250afc17fd8759983658e739
Jtata123 <86513833+Jtata123@users.noreply.github.com>
2022-03-25 13:21:52 +0800

    Delete FaceHistoryContainer.jsx

--- FaceHistoryContainer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import FaceHistory from "../components/FaceHistory";
-
-const FaceHistoryContainer = () => (
-    <FaceHistory/>
-)
-
-export default FaceHistoryContainer;
